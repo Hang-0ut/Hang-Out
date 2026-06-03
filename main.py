67c0c694095ad470859271f96416a9a7384b6198
Jacob1Carter <jacob1carter@outlook.com>
2026-06-03 01:18:26 +0100

    Basic Pages

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
@@ -6,6 +6,26 @@ app = Flask(__name__)
 @app.route("/")
 def landing():
     return render_template("landing.html")
+
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+
+@app.route("/register-input", methods=["POST"])
+def register_input():
+    return redirect(url_for("/"))
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+@app.route("/login-input", methods=["POST"])
+def login_input():
+    return redirect(url_for("/"))
 
 
 if __name__ == "__main__":
