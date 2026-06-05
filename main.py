47f20244d6374192a47a215aca4f43b6f1cad355
Jacob1Carter <jacob1carter@outlook.com>
2026-06-05 02:00:16 +0100

    Restructure

--- main.py
+++ main.py
@@ -5,27 +5,27 @@ app = Flask(__name__)
 
 @app.route("/")
 def landing():
-    return render_template("landing.html")
+    return render_template("main/landing.html")
 
 
 @app.route("/register")
 def register():
-    return render_template("register.html")
+    return render_template("main/register.html")
 
 
 @app.route("/register-input", methods=["POST"])
 def register_input():
-    return redirect(url_for("/"))
+    return redirect(url_for("main/landing"))
 
 
 @app.route("/login")
 def login():
-    return render_template("login.html")
+    return render_template("main/login.html")
 
 
 @app.route("/login-input", methods=["POST"])
 def login_input():
-    return redirect(url_for("/"))
+    return redirect(url_for("main/landing"))
 
 
 if __name__ == "__main__":
